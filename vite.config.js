88d7a5ef374e003f2843ac85e78866e78bc5839f
lnh2020 <331137909@qq.com>
2026-03-27 16:21:31 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, 'src')
+export default defineConfig(({mode}) => {
+  console.log(`output->mode`,mode)
+  return {
+    plugins: [vue()],
+    base: mode === 'development' ? './' : '/exam/',
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, 'src')
+      }
     }
   }
 })
